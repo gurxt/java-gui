beef02ab0e7a02192e709e0ffbca49f463adb8d3
gurxt <jung_jungian@outlook.com>
2023-02-13 16:16:44 -0500

    example of JFrame

---
+++ Example.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Example {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("click");
+        button.setBounds(130, 100, 100, 40);
+        frame.add(button);
+        frame.setSize(400, 500);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
