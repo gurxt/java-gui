665ad8c01ea47dca8db2b2b5aa1164ef87891a5e
gurxt <jung_jungian@outlook.com>
2023-02-13 16:52:53 -0500

    set basic frame

--- Example.java
+++ Example.java
@@ -1,13 +1,35 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Example {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        JButton button = new JButton("click");
-        button.setBounds(130, 100, 100, 40);
-        frame.add(button);
-        frame.setSize(400, 500);
-        frame.setLayout(null);
-        frame.setVisible(true);
+        Example example = new Example();
+        example.setFrame(frame);
+    }
+
+    private void setFrame(JFrame _frame) {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int) screenSize.getWidth();
+        int height = (int) screenSize.getHeight();
+        _frame.setSize(width, height);
+        _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        _frame.setLayout(new FlowLayout(FlowLayout.CENTER));
+        _frame.setVisible(true);
+        _frame.getContentPane().setBackground(Color.BLACK);
     }
 }
+
+/*
+public class Button {
+    JButton button = new JButton("hello");
+    public Button(JFrame _frame, int _idx) {
+        button.setBounds(130, 100 * (_idx + 1), 100, 40);
+        _frame.add(button);
+    }
+    
+    public JButton getButton() {
+        return button;
+    }
+}
+*/
