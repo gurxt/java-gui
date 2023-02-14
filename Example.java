d505cc1d7fbc78c344f1cfc10e0348d79b9170a8
gurxt <jung_jungian@outlook.com>
2023-02-13 20:16:28 -0500

    wheres my handsome tim/

--- Example.java
+++ Example.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class Example {
@@ -14,28 +15,50 @@ public class SwingExample extends JFrame {
     /* constructor */
     public SwingExample() {
         /* set a label */
-        this.setLabel();
+        this.setLabel(0, 0);
+        this.setLabel(502, 0);
+        this.setLabel(0, 500);
+        this.setLabel(502, 500);
+        this.setNone();
         this.setFrame();
     }
 
-    public void setFrame() {
+    private void setFrame() {
         /* JFrame = a GUI window */
         this.setTitle("JFrame Example");
-        this.setSize(420,420);
-        this.setResizable(false);
+        this.setSize(1000,1000);
+        this.setResizable(true);
         this.setVisible(true);
+        this.setLayout(null);
         this.getContentPane().setBackground(new Color(0x444444));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         /* change image icon. */
         this.setIconImage(image.getImage());
     }
 
-    public void setLabel() {
+    private void setLabel(int _x, int _y) {
         JLabel label = new JLabel();
+        Border border = BorderFactory.createLineBorder(Color.green, 2);
+
         label.setIcon(image);
         label.setText("Hello World.");
         label.setForeground(new Color(0xFFFFFF));
-        label.setHorizontalTextPosition(JLabel.LEFT);
+        label.setBackground(new Color(0x000000));
+        label.setFont(new Font("MV Boli", Font.BOLD, 20));
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+        label.setIconTextGap(10);
+        label.setOpaque(true);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setBorder(border);
+        label.setBounds(_x, _y, 500, 500);
+
+        this.add(label);
+    }
+
+    private void setNone() {
+        JLabel label = new JLabel();
         this.add(label);
     }
 }
